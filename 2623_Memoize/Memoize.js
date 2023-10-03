5227204510845352239afe085c876cf7c4bad3f5
Kanayo Emenike <36934634+Kemenike@users.noreply.github.com>
2023-10-03 14:53:58 -0700

    Create Memoize.js
    Result for 2623 Memoization. 10-3-2023

---
+++ 2623_Memoize/Memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function (...args) {
+        if(cache.get(JSON.stringify(args)) === undefined) {
+            const result = fn(...args);
+            cache.set(JSON.stringify(args), result);
+            return result;
+        } else {
+            return cache.get(JSON.stringify(args));
+        }
+
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
